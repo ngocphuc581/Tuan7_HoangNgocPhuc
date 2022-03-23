119933e5f915264a7613bb6f566e2e3e21f33ea3
phuc <ngocphuc00001@gmail.com>
2022-03-23 12:40:07 +0700

    tuan7 hoangngocphuc

---
+++ Tuan4_HoangNgocPhuc/Controllers/NguoiDungController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tuan4_HoangNgocPhuc.Models;
+using FormCollection = System.Web.Mvc.FormCollection;
+
+namespace Tuan4_HoangNgocPhuc.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        // GET: NguoiDung
+        MyDataDataContext data = new MyDataDataContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangKy(FormCollection collection, KhachHang kh)
+        {
+            var hoten = collection["hoten"];
+            var tendangnhap = collection["tendangnhap"];
+            var matkhau = collection["matkhau"];
+            var MatKhauXacNhan = collection["MatKhauXacNhan"];
+            var email = collection["email"];
+            var diachi = collection["diachi"];
+            var dienthoai = collection["dienthoai"];
+            var ngaysinh = String.Format("{0:MM/dd/yyyy}", collection["ngaysinh"] );
+            if (String.IsNullOrEmpty(MatKhauXacNhan))
+            {
+                ViewData["NhapMKXN"] = "Phải nhập mật khẩu xác nhận";
+            }
+            else
+            {
+                if (!matkhau.Equals(matkhau))
+                {
+                    ViewData["MatKhauGiongNhau"] = "Mat Khau Xac Nhan Phai Giong Nhau";
+                }
+                else
+                {
+                    kh.hoten = hoten;
+                    kh.tendangnhap = tendangnhap;
+                    kh.matkhau = matkhau;
+                    kh.email = email;
+                    kh.diachi = diachi;
+                    kh.dienthoai = dienthoai;
+                    kh.ngaysinh = DateTime.Parse(ngaysinh);
+                    data.KhachHangs.InsertOnSubmit(kh);
+                    data.SubmitChanges();
+
+                    return RedirectToAction("DangNhap");
+                }
+            }
+            return this.DangKy();
+        }
+        [HttpGet]
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection collection)
+        {
+            var tendangnhap = collection["tendangnhap"];
+            var matkhau = collection["matkhau"];
+            KhachHang kh = data.KhachHangs.SingleOrDefault(n => n.tendangnhap.Equals(tendangnhap) && n.matkhau.Equals(matkhau));
+            if(kh != null)
+            {
+                ViewBag.ThongBao = "Chuc mung ban da login thanh cong";
+                Session["TaiKhoan"] = kh;
+            }
+            else
+            {
+                ViewBag.ThongBao = "Ten dang nhap hoac mat khau khong dung";
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
---
+++ Tuan4_HoangNgocPhuc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tuan4_HoangNgocPhuc.Startup))]
+namespace Tuan4_HoangNgocPhuc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
